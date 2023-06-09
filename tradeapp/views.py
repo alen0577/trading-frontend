4a85e7296f474772fe7f25019d0cc817e0430cee
Alen Antony <alenantony32@gmail.com>
2023-06-09 21:11:17 +0530

    second commit

--- tradeapp/views.py
+++ tradeapp/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def signin(request):
+    return render(request,'signin.html')  
+
+def signup(request):
+    return render(request,'signup.html')       
